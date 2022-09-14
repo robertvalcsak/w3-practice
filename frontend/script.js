8a25875d2256a60109b3c6b30bbf67ed8e381a08
Róbert Valcsák <robertvalcsak93@gmail.com>
2022-09-14 11:42:55 +0200

    variables

---
+++ frontend/script.js
@@ -0,0 +1,21 @@
+// var myVariable;  soha ne használj a var-t!
+let myVariable;
+console.log(myVariable);
+
+myVariable = "new value";
+console.log(myVariable);
+
+myVariable = 2;
+console.log(myVariable);
+
+myVariable = true;
+console.log(myVariable);
+
+let myVariable2 = "szöveg";
+console.log(myVariable2);
+
+myVariable = myVariable2;
+console.log(myVariable);
+
+let myVariable3 = myVariable + myVariable2
+console.log(myVariable3);
