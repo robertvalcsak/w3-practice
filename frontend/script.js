76c5882c2bcbaba0056e49383143612736cf0535
Róbert Valcsák <robertvalcsak93@gmail.com>
2022-09-14 12:15:38 +0200

    js showcase, function, loadevent

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,36 @@
-// var myVariable;  soha ne használj a var-t!
+// var myVariable; soha ne használj var-t!!!
 let myVariable;
-console.log(myVariable);
+// console.log(myVariable);
 
 myVariable = "new value";
-console.log(myVariable);
+// console.log(myVariable);
 
 myVariable = 2;
-console.log(myVariable);
+// console.log(myVariable);
 
 myVariable = true;
-console.log(myVariable);
+// console.log(myVariable);
 
 let myVariable2 = "szöveg";
-console.log(myVariable2);
+// console.log(myVariable2);
 
 myVariable = myVariable2;
-console.log(myVariable);
+// console.log(myVariable);
 
-let myVariable3 = myVariable + myVariable2
-console.log(myVariable3);
+let myVariable3 = myVariable + myVariable2;
+// console.log(myVariable3);
+
+function sumVariables(variable1, variable2) {
+    console.log(variable1 + variable2);
+}
+sumVariables(myVariable, myVariable2);
+sumVariables(8, "nyolc");
+sumVariables("word1","word2");
+
+function loadEvent() {
+    console.log("loaded");
+    let rootElement = document.getElementById("root");
+    rootElement.insertAdjacentHTML("beforeend", "hello world");
+}
+
+window.addEventListener("click", loadEvent());
