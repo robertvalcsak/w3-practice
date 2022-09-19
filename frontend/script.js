2e1e9dcc141b386fa34d8b146fd7788e20ab8965
Róbert Valcsák <robertvalcsak93@gmail.com>
2022-09-19 19:58:21 +0200

    js changed

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,10 @@
 function loadEvent(){
-    const newCatButton = document.querySelector("add-cat");
-newCatButton.addEventListener("click", function() {
-    const catContainer = document.querySelector(".cats")
-    const now = new Date().getTime();
-    const newCat = `<img src="https://cataas.com/cat?random=${now}" alt="">`;
-    catContainer.insertAdjacentHTML("beforeend", newCat);
+    const newCatButton = document.querySelector(".add-cat");
+    newCatButton.addEventListener("click", function() {
+        const catContainer = document.querySelector(".cats")
+        const now = new Date().getTime();
+        const newCat = `<img src="https://cataas.com/cat?random=${now}" alt="">`;
+        catContainer.insertAdjacentHTML("beforeend", newCat);
     })
 }
 
